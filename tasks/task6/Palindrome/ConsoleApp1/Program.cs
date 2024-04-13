6129ba3d58407406577e6b077321064481f6351d
Ahmed Abdou <150970091+AhmedMAbdou@users.noreply.github.com>
2024-04-14 01:28:49 +0200

    Add files via upload

---
+++ tasks/task6/Palindrome/ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //code for achieve from if word is palindrome or no
+            while (true)
+            {
+                Console.Write("Enter your word: ");
+                string word = Console.ReadLine();
+                bool test = true;
+                for (int i = 0; i < word.Length / 2; i++)
+                {
+                    if (word[i] == word[word.Length - 1 - i])
+                    {
+                        test = true;
+                    }
+                    else
+                        test = false;
+                }
+                Console.WriteLine(test);
+            }
+        }
+    }
+}
