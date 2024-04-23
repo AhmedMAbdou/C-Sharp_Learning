895b13fa1bcd508a54f11b55fcb1bcdc057aa127
Ahmed Abdou <150970091+AhmedMAbdou@users.noreply.github.com>
2024-04-23 03:24:34 +0200

    Add files via upload

---
+++ tasks/task7/SwitchWords/ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter original name: ");
+            string origingal = Console.ReadLine();
+            Console.Write("Enter old name: ");
+            string old = Console.ReadLine();
+            Console.Write("Enter new name: ");
+            string _new = Console.ReadLine();
+            Console.WriteLine($"The Result is: {origingal.Replace(old, _new, StringComparison.OrdinalIgnoreCase)}");
+        }
+    }
+}
