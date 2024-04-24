873d72d705718ceaef625d9c15b7dbda27a15585
Ahmed Abdou <150970091+AhmedMAbdou@users.noreply.github.com>
2024-04-24 09:35:13 +0200

    Add files via upload

---
+++ tasks/task8/Ascii-code/ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var all = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+            foreach (var x in all)
+            {
+                Console.WriteLine((int)x);
+            }
+        }
+    }
+}
