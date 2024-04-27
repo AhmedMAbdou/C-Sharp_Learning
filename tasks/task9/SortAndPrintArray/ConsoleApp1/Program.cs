d3e174faa063699cdb27da38be6d177891ce081a
Ahmed Abdou <150970091+AhmedMAbdou@users.noreply.github.com>
2024-04-27 06:43:04 +0300

    Add files via upload

---
+++ tasks/task9/SortAndPrintArray/ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[5];
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Write("Enter your num: ");
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("your array is: ");
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+        }
+    }
+}
