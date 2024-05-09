eb109e4a4606d4320666845f15e2f9c277845bc0
Ahmed Abdou <150970091+AhmedMAbdou@users.noreply.github.com>
2024-05-09 14:11:55 +0300

    Add files via upload

---
+++ tasks/task11/ForLoop/ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the numbers of elements of array: ");
+            string numbers = Console.ReadLine();
+            int new_numbers = int.Parse(numbers);
+            //this line=two lines up {int new_numbers = int.Parse(Console.ReadLine())};
+            string[] arr = new string[new_numbers];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write("Enter your array: ");
+                arr[i] = Console.ReadLine();
+                int new_arr = int.Parse(arr[i]);
+            }
+            //Array.Reverse(arr);
+            Console.WriteLine("-------------------------------");
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+        }
+    }
+}
